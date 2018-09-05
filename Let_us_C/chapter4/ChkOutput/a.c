a35fa154c9f411dfbeca989b8aedce02c0ec856a
Biswajit Mahanta <tunein2biswa@gmail.com>
2018-09-05 15:55:55 +0530

    Add test files to checkout the output

---
+++ Let_us_C/chapter4/ChkOutput/a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int i=4, z=12;
+	if(i=4||z>50)
+		printf("\n Dean of student affairs");
+	else
+		printf("\n Dosa");
+
+}
---
+++ Let_us_C/chapter4/tertiary_check.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=3, b=0;
+	
+	(a<1)?(b=1):0;
+	
+	printf("%d", b);
+
+	return 0;
+}
