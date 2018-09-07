cafb686d41dc182bb6a3505216857267b62718a6
Biswajit Mahanta <tunein2biswa@gmail.com>
2018-09-07 15:39:01 +0530

    Modified codes for Pascal's triangle

--- Let_us_C/chapter6/charPattern2.c
+++ Let_us_C/chapter6/charPattern2.c
@@ -1,15 +1,19 @@
+// Pascal's triangle
+
 #include <stdio.h>
 
 int main()
 {
-	int n, i, j, flag, row, x;
+	int i, j, diff, row, x;
 
 	printf("Enter required no of row of the pattern\n");
 	scanf("%d", &row);
 
 	for (i=1; i<=row; i++)
 	{
-		x = i-1; // Every row will start with followed by x
+		diff = 1;
+		x = i-2; 
+
 		for (j = 1; j < 2*row; j++ )
 		{
 			if (j <= row-i) 	//Starting blank space
@@ -23,32 +27,23 @@ int main()
 			
 			else			//All numbers are in here
 			{
+				if (j == row)
+					diff = -1;
+
 				//first and last positions are always 1
 				if ((j == row-i+1) || (j == row+i-1))
 					printf("1");
 			
-				//in even lines odd positions are blank and 
-				//in odd iines even posiitions are blank
-				else if( (   j%2  && !(i%2) ) || 
-					 ( !(j%2) &&   i%2  ) )
+				//odd positions from stating char are blank
+				else if ((j - (row-i+1)) % 2)
+				{
 					printf(" ");
-
-				else if (j < row)
-				{
-					printf("%d", x);
-					x += 2;
+					x += diff;
 				}
-
-				else if (j%2 == 0) 
-				{
-					x -= 2;
-					printf("%d", x);
-				}
-
 				else 
 				{
 					printf("%d", x);
-					x -= 2;
+					x += diff;
 				}
 			}
 		}
