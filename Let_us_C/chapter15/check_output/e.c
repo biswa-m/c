f2002576418c3dec24f6c5c26de8bdd6037c13e8
biswa-m <tunein2biswa@gmail.com>
2018-09-27 03:05:50 +0530

    Add code to get familiar with basic characteristics of String

---
+++ Let_us_C/chapter15/check_output/e.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	char str1[] = {'H', 'e', 'l', 'l', 'o'};
+	char str2[] = "Welcome";
+
+	printf("\n%s", str1);
+	printf("\n%s", str2);
+}
---
+++ Let_us_C/chapter15/check_output/h.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("\n%d%d%d", sizeof('3'), sizeof("3"), sizeof(3));
+}
